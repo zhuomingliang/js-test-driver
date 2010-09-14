09a52b526dc705af9afdba6cc08b788a24028968
aeagle22206@gmail.com <aeagle22206@gmail.com@205c1ca0-44cf-11de-84a6-839b6ed6c72e>
2010-09-14 19:02:00 +0000

    Apply the patch from Dariusz Kordonski (dariusz.kordonski@googlemail.com) to the 1.2.2 branch. This gets the idea-plugin working on IDEA version 9.
    Fixed the ant build as well. This may be broken on Misko's machine since IDEA9 isn't present, and the hudson config doesn't include a path to the idea.jar for use in Ant. Someone will need to fix that :)
    
    Right now, I can run the plugin in the sandbox IDE and successfully start the server, capture a browser, and run tests. So we are basically working. Next step is to fix a GUI call off the AWT thread, do some more testing, and cut a release of the plugin.
    
    Update issue 82

---
+++ idea-plugin/src/com/google/jstestdriver/idea/TestListenerContext.java
@@ -0,0 +1,46 @@
+package com.google.jstestdriver.idea;
+
+import com.intellij.execution.process.ProcessHandler;
+import com.intellij.execution.testframework.sm.runner.ui.SMTRunnerConsoleView;
+import com.intellij.execution.testframework.sm.runner.ui.SMTestRunnerResultsForm;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.Future;
+
+/**
+ * @author Dariusz Kordonski (dariusz.kordonski@googlemail.com)
+ */
+public final class TestListenerContext {
+
+  private final Future<TestRunnerState.ProcessData> processData;
+  private final CountDownLatch gate;
+
+  public TestListenerContext(Future<TestRunnerState.ProcessData> processData, CountDownLatch gate) {
+    this.processData = processData;
+    this.gate = gate;
+  }
+
+  public void socketStarted() {
+    gate.countDown();
+  }
+
+  public SMTestRunnerResultsForm resultsForm() {
+    return consoleView().getResultsViewer();
+  }
+
+  public SMTRunnerConsoleView consoleView() {
+    return getData().consoleView;
+  }
+
+  public ProcessHandler processHandler() {
+    return getData().processHandler;
+  }
+
+  private TestRunnerState.ProcessData getData() {
+    try {
+      return processData.get();
+    } catch (Exception e) {
+      throw new RuntimeException(e);
+    }
+  }
+}
